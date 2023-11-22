d37f632ff512907cbbb079e2340d8a2d6477cb9e
John Correa <jecl2912@gmail.com>
2023-11-21 23:05:58 -0500

    fix: header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,28 @@
 import { Stack } from 'expo-router';
-import Menu from '../components/Menu';
+import { View, Text } from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={{ flexDirection: 'column', margin: 15 }}>
+            <Text>Header</Text>
+            <Text>Header</Text>
+            <Text>Header</Text>
+            <Text>Header</Text>
+        </View>
+    )
+}
 
 export default function Layout() {
     return (
         <Stack
             screenOptions={{
-                headerShown: false
-            }}
-        />
+                headerShown: true,
+                headerTitle: (props) => (
+                    <Text>Header</Text>
+                ),
+                /*  header: () => <Header />, */
+                headerTintColor: 'transparent',
+                headerBackTitleVisible: false,
+            }} />
     );
 }
